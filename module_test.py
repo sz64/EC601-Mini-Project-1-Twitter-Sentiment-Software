955529fd508a42fb432f02e0932277c32d678eef
sz64 <43184828+sz64@users.noreply.github.com>
2019-09-13 14:26:18 -0400

    Add files via upload

--- module_test.py
+++ module_test.py
@@ -1,6 +1,6 @@
 import tweepy
 import tweet_puller
-import google_sentiments
+import sentiment_puller
 
 scr_name = input('Twitter username: ');
 n_tweets = int(input('How many tweets would you like to analyze? '))
@@ -11,5 +11,5 @@ tweets = tweet_puller.tweet_pull(scr_name, n_tweets);
 sentiments_list = [];
 
 for n in tweets:
-	sentiments_list.append(google_sentiments.get_sentiment(n));
-	print(google_sentiments.get_sentiment(n));
+	sentiments_list.append(sentiment_puller.get_sentiment(n));
+	print(sentiment_puller.get_sentiment(n));
--- tweet_puller.py
+++ tweet_puller.py
@@ -1,6 +1,8 @@
 import tweepy
 
 def tweet_pull(scr_name, n_tweets):
+
+
 	keys = open("TKey.txt").read().split();
 	consumer_key = keys[0];
 	consumer_secret = keys[1];
@@ -8,7 +10,6 @@ def tweet_pull(scr_name, n_tweets):
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
 	api = tweepy.API(auth)
-	n = 0;
 
 	def limit_handled(cursor):
 		while True:
@@ -17,9 +18,8 @@ def tweet_pull(scr_name, n_tweets):
 			except tweepy.RateLimitError:
 				time.sleep(15 * 60)
 
-	n = 0;
 	tweets = [];
 	for tweet in limit_handled(tweepy.Cursor(api.user_timeline, screen_name=scr_name, result_type = 'recent', tweet_mode='extended').items(n_tweets)):
-		tweets.append(tweet.full_text); n = n + 1;
+		tweets.append(tweet.full_text);
 		
 	return tweets;
