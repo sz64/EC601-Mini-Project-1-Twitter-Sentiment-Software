e3a7e90d9519451dbf15c356a73568ba4cd5ec2a
sz64 <43184828+sz64@users.noreply.github.com>
2019-09-21 16:01:56 -0400

    Update tweet_puller.py

--- tweet_puller.py
+++ tweet_puller.py
@@ -11,15 +11,8 @@ def tweet_pull(scr_name, n_tweets):
 
 	api = tweepy.API(auth)
 
-	def limit_handled(cursor):
-		while True:
-			try:
-				yield cursor.next()
-			except tweepy.RateLimitError:
-				time.sleep(15 * 60)
-
 	tweets = [];
-	for tweet in limit_handled(tweepy.Cursor(api.user_timeline, screen_name=scr_name, result_type = 'recent', tweet_mode='extended').items(n_tweets)):
+	for tweet in tweepy.Cursor(api.user_timeline, screen_name=scr_name, result_type = 'recent', tweet_mode='extended').items(n_tweets):
 		tweets.append(tweet.full_text);
 		
 	return tweets;
